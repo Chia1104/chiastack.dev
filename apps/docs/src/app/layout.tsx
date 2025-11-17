88d0c814edfc253218d8a220b207280fa6cb40d1
Chia1104 <yuyuchia7423@gmail.com>
2025-11-17 23:10:36 +0800

    feat(tsconfig): update ts config

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
 import { Inter } from "next/font/google";
 
+import { initDayjs } from "@chiastack/utils/day";
+
 import "./global.css";
 
 const inter = Inter({
@@ -8,6 +10,7 @@ const inter = Inter({
 });
 
 export default function Layout({ children }: LayoutProps<"/">) {
+  initDayjs();
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
