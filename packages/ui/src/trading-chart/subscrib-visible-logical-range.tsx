e62981df471a148ee88c6523d35e107d5fd79b76
Chia1104 <yuyuchia7423@gmail.com>
2025-11-26 14:10:16 +0800

    feay(ui): indroduce new trading-chart package

---
+++ packages/ui/src/trading-chart/subscrib-visible-logical-range.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useEffect, useCallback } from "react";
+
+import type { LogicalRange } from "lightweight-charts";
+
+import { useChart } from "./chart";
+
+interface Props {
+  method: (range: number) => void | Promise<void>;
+  enabled?: boolean;
+}
+
+export const SubscribeVisibleLogicalRange = ({
+  method,
+  enabled = true,
+}: Props) => {
+  const chart = useChart();
+
+  const handleSubscribeVisibleLogicalRangeChange = useCallback(
+    (logicalRange: LogicalRange | null) => {
+      if (!logicalRange || !enabled) {
+        return;
+      }
+      if (logicalRange.from < 10) {
+        void method(logicalRange.from);
+      }
+    },
+    [enabled, method]
+  );
+
+  useEffect(() => {
+    const api = chart.api();
+    if (enabled) {
+      api
+        .timeScale()
+        .subscribeVisibleLogicalRangeChange(
+          handleSubscribeVisibleLogicalRangeChange
+        );
+    } else {
+      api
+        .timeScale()
+        .unsubscribeVisibleLogicalRangeChange(
+          handleSubscribeVisibleLogicalRangeChange
+        );
+    }
+
+    return () => {
+      api
+        .timeScale()
+        .unsubscribeVisibleLogicalRangeChange(
+          handleSubscribeVisibleLogicalRangeChange
+        );
+    };
+  }, [chart, enabled, handleSubscribeVisibleLogicalRangeChange]);
+
+  return null;
+};
--- packages/ui/tsdown.config.ts
+++ packages/ui/tsdown.config.ts
@@ -1,4 +1,25 @@
+import { readFile, writeFile } from "node:fs/promises";
 import { defineConfig, type UserConfig } from "tsdown";
+
+type PackageJson = {
+  name: string;
+  exports: Record<string, { import: string; types: string } | string>;
+  typesVersions: Record<"*", Record<string, string[]>>;
+  files: string[];
+  dependencies: Record<string, string>;
+  pnpm?: {
+    overrides: Record<string, string>;
+  };
+};
+
+const ESEntries = [
+  "./src/trading-chart/chart.tsx",
+  "./src/trading-chart/series.tsx",
+  "./src/trading-chart/macd-series.tsx",
+  "./src/trading-chart/rsi-series.tsx",
+  "./src/trading-chart/subscrib-visible-logical-range.tsx",
+  "./src/error-boundry/error-boundary.tsx",
+];
 
 export default defineConfig((opts) => {
   const common = {
@@ -12,9 +33,40 @@ export default defineConfig((opts) => {
   return [
     {
       ...common,
-      entry: ["./src/index.ts"],
-      banner: {
-        js: '"use client";',
+      entry: ESEntries,
+      async onSuccess() {
+        const pkgJson = JSON.parse(
+          await readFile("./package.json", {
+            encoding: "utf-8",
+          })
+        ) as PackageJson;
+        pkgJson.exports = {
+          "./package.json": "./package.json",
+        };
+        pkgJson.typesVersions = {
+          "*": {},
+        };
+        [...ESEntries]
+          .filter((e) => e.endsWith(".tsx") || e.endsWith(".ts"))
+          .forEach((entry) => {
+            const file =
+              entry
+                .split("/")
+                .pop()
+                ?.replace(/\.(tsx|ts)$/, "") ?? "";
+            const relativePath = entry
+              .replace(/^\.\/src\//, "")
+              .replace(/\.(tsx|ts)$/, "");
+            pkgJson.exports["./" + file] = {
+              import: "./dist/" + relativePath + ".mjs",
+              types: "./dist/" + relativePath + ".d.mts",
+            };
+            pkgJson.typesVersions["*"][file] = [
+              "./dist/" + relativePath + ".d.mts",
+            ];
+          });
+
+        await writeFile("./package.json", JSON.stringify(pkgJson, null, 2));
       },
     },
   ] satisfies UserConfig[];
